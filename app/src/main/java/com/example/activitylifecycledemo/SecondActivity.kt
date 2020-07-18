0071a52d1c6cbfba399ce72c950c5beab742388d
sougata-nayak <sougatanayak007@gmail.com>
2020-07-18 12:42:26 +0530

    Third activity and its intent added

--- app/src/main/java/com/example/activitylifecycledemo/SecondActivity.kt
+++ app/src/main/java/com/example/activitylifecycledemo/SecondActivity.kt
@@ -1,11 +1,23 @@
 package com.example.activitylifecycledemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        btnThirdActivity.setOnClickListener {
+            Intent(this, ThirdActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        btnMainActivity.setOnClickListener {
+            finish()
+        }
     }
 }
