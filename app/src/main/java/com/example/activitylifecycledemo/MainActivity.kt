24330ea6b04b57c6dcb3793cb633d3bb36e893c8
sougata-nayak <sougatanayak007@gmail.com>
2020-07-18 12:09:52 +0530

    Second activity and its intent added

--- app/src/main/java/com/example/activitylifecycledemo/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycledemo/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.activitylifecycledemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSecondActivity.setOnClickListener{
+            Intent(this, SecondActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
